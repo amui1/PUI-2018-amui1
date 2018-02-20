21226dc1a18888b735fac590e18f9b9df84f5dcb
Allison Mui <allisonmui@Allisons-Air-2.wv.cc.cmu.edu>
2018-02-20 11:06:59 -0500

    Week 6 Part 1

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -2,17 +2,17 @@ function addNewList() {
     alert('hello world!');
 }
 
-function addListItem() {
-    var list = document.getElementById("grocery-list");
-    var itemInput = document.getElementById("new-list-item");
-    var newItem = document.createElement("li");
-    newItem.appendChild(document.createTextNode(itemInput.value));
-    list.appendChild(newItem);
-}
-
-function deleteListItem(item) {
-    item.parentNode.removeChild(item);
-}
+// function addListItem() {
+//     var list = document.getElementById("grocery-list");
+//     var itemInput = document.getElementById("new-list-item");
+//     var newItem = document.createElement("li");
+//     newItem.appendChild(document.createTextNode(itemInput.value));
+//     list.appendChild(newItem);
+// }
+//
+// function deleteListItem(item) {
+//     item.parentNode.removeChild(item);
+// }
 
 function completeListItem(item) {
     if (item.checked) {
@@ -22,3 +22,26 @@ function completeListItem(item) {
       item.parentNode.style.textDecoration = "none";
     }
 }
+
+// $(document).ready(function(){
+//     $("#add-item").click(function() { // bind handler for click event
+//         var list = $("#grocery-list"); // get the ol list by id
+//         var itemInput = $("#new-list-item"); // get the new item input
+// 	  // append the input value within an li element
+//         list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>")
+//     });
+//     $(".delete-item").click(function() {
+//         console.log("test");
+//         $(this).parent().remove();
+//     });
+// });
+
+$(document).on("click", ".delete-item", function() {
+    $(this).parent().remove();
+});
+
+$(document).on("click", "#add-item", function() {
+    var list = $("#grocery-list");
+    var itemInput = $("#new-list-item");
+    list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+});
