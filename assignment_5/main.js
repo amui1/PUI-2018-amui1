1bd81300fcf158cea12e6bbc51eb51e19d785b29
Allison Mui <allisonmui@Allisons-MacBook-Air-2.local>
2018-03-27 21:51:46 -0400

    Assignment_5

--- assignment_5/main.js
+++ assignment_5/main.js
@@ -1,67 +1,72 @@
-var order = [];
-var product = [];
-var complete = false;
+
 var cartnum = 0;
 
-$(function() {
 
+var color;
+var size;
+
+// make product when user selects
+
+function add_Collar(name,color,size,qty) {
+  this.name = 'Dog GPS Tracker Collar';
+  this.color = color;
+  this.size = size;
+  this.qty = cartnum;
+  this.image = "assets/images/collar.jpg";
+}
+
+
+$(document).ready(function() {
+  // start with empty array for user's order
+  var order = [];
+
+  // when user clicks blue button
   $('.blue').click(function() {
-
+    // change the display of the blue button
     $(this).css('border',"solid 6px #234164");
-
-    product.push(this);
-
+    // assign a color
+    color = 'Crazyberry';
+    // change the picture
     $("#product_pic").attr('src',"assets/images/collar.jpg");
     return false;
-
   })
 
+  // when the user clicks the medium button
   $('.medium').click(function() {
+    // change the display
     $(this).css('background-color',"#F28A91");
-    product.push(this);
-    complete = true;
+    // assign a size
+    size = 'medium';
   })
+
+  // when the user clicks the add to cart button
+  $('.add').click(function() {
+    // increase the number of items in the cart
+    cartnum += 1;
+    // display this on the site by linking to the HTML via id
+    document.getElementById("cart_num").innerHTML = cartnum;
+
+    // assign user's product to a variable
+    var cur_product = new add_Collar(name,color,size,1);
+    // push that variable to the user's entire order
+    order.push(cur_product);
+    // add this to local storage
+    localStorage.setItem("order",JSON.stringify(order));
+  })
+
+  // display attributes on the website
+  // parses through objects in localStorage
+  // indexes at the first one to access the first object in localStorage
+  // accesses attributes by calling the first object's parameter
+  $("#idv_pic").attr("src", JSON.parse(localStorage["order"])[0].image);
+  $("#item_name").text(JSON.parse(localStorage["order"])[0].name);
+  $("#item_color").text(JSON.parse(localStorage["order"])[0].color);
+  $("#item_size").text(JSON.parse(localStorage["order"])[0].size);
+  // figures out how many items the user ordered assuming the user only orders the exact same item
+  $("#qty").text('Quantity : ' + JSON.parse(localStorage["order"])[Math.floor(localStorage.order.length/113 -1)].qty);
+
+
+
+
 
 })
-
-
-
-function cart() {
-  if (complete == false) {
-    alert('Please fill out all options!');
-    console.log('hi');
-  }
-  if (complete == true) {
-    cartnum += 1;
-    product.push(cartnum);
-
-    if (order.length == 0) {
-      order.push(product);
-    }
-
-    else {
-      for (i = 0; i < order.length; i++) {
-        for (j = 0; j < order[i].length; i++) {
-          if (product[0] != order[i][j]) {
-            order.push(product);
-
-          }
-          else {
-            product[3] = cartnum;
-            order.push(product);
-
-          }
-        }
-      }
-    }
-
-    console.log(product);
-    console.log(order);
-
-
-
-
-
-    document.getElementById("cart_num").innerHTML = cartnum;
-  }
-}
